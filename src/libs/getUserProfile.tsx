a7f20704fa3333bc3b60934b9eb0eae1066f9381
FreezingH2O <airadajitauleeruk@gmail.com>
2025-04-29 13:46:55 +0700

    fix error3

--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,4 +1,4 @@
-yexport default async function getUserProfile(token: string) {
+export default async function getUserProfile(token: string) {
 
     const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/me`, {
         method: 'GET',
